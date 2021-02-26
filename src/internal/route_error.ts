4577e2c09b219cdd894561922d4fb54600024b39
Jeongho Nam <samchon@samchon.org>
2021-02-26 11:09:13 +0900

    route_error be a little bit faster

--- src/internal/route_error.ts
+++ src/internal/route_error.ts
@@ -1,10 +1,18 @@
+import { HttpException } from "@nestjs/common";
 import { Observable, throwError } from "rxjs";
 import { ExceptionManager } from "../ExceptionManager";
 
 export function route_error(error: any): Observable<never>
 {
+    // HTTP-ERROR
+    if (error instanceof HttpException)
+        return throwError(error);
+
+    // CUSTOM-REGISTERED ERROR
     for (const tuple of ExceptionManager.dictionary)
         if (error instanceof tuple[0])
             return throwError(tuple[1](error));
+    
+    // MAYBE INTERNAL ERROR
     return throwError(error);
 }
