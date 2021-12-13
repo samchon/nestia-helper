9d69e67a5449d6b90db1f4a926ba32b162c5ea3e
Jeongho Nam <samchon.github@gmail.com>
2021-12-13 13:41:42 +0900

    Add `PlainBody`

--- src/EncryptedBody.ts
+++ src/EncryptedBody.ts
@@ -8,13 +8,13 @@ import { HttpException } from "@nestjs/common";
 
 export const EncryptedBody: (() => ParameterDecorator) = nest.createParamDecorator
 (
-    async function EncryptedBody({}: any, ctx: nest.ExecutionContext)
+    async function EncryptedBody({}: any, context: nest.ExecutionContext)
     {
-        const request: express.Request = ctx.switchToHttp().getRequest();
+        const request: express.Request = context.switchToHttp().getRequest();
         if (request.readable === false)
             throw new HttpException("Request body is not the text/plain.", 500);
 
-        const param: IPassword | IPassword.Closure = Reflect.getMetadata("encryption:password", ctx.getClass());
+        const param: IPassword | IPassword.Closure = Reflect.getMetadata("encryption:password", context.getClass());
         const content: string = (await raw(request, "utf8")).trim();
         const config: IPassword = (param instanceof Function)
             ? param(content, false)
---
+++ src/PlainBody.ts
@@ -0,0 +1,16 @@
+import * as express from "express";
+import raw from 'raw-body';
+import { createParamDecorator, ExecutionContext, BadRequestException } from '@nestjs/common';
+
+export const PlainBody: (() => ParameterDecorator) = createParamDecorator
+(
+    async (_data: any, context: ExecutionContext) => 
+    {
+        const requeest: express.Request = context.switchToHttp().getRequest();
+        if (!requeest.readable)
+            throw new BadRequestException("Invalid body");
+
+        const body: string = (await raw(requeest)).toString("utf8").trim();
+        return body;
+    }
+);
